726c9ce13a9f73c61baee8a596761638b23b16ed
raphtalia <alexander5082t@gmail.com>
2020-02-22 20:34:47 -0600

    indexorMotor connected to indexerSpeed

--- wpi/src/main/java/com/first1444/frc/robot2020/MotorIntake.java
+++ wpi/src/main/java/com/first1444/frc/robot2020/MotorIntake.java
@@ -1,10 +1,18 @@
 package com.first1444.frc.robot2020;
 
 import com.first1444.frc.robot2020.subsystems.implementations.BaseIntake;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 public class MotorIntake extends BaseIntake {
+    private final CANSparkMax indexerMotor;
+
+    public MotorIntake() {
+        indexerMotor = new CANSparkMax(40, CANSparkMaxLowLevel.MotorType.kBrushless);
+    }
+
     @Override
     protected void run(double intakeSpeed, double indexerSpeed, double feederSpeed) {
-        throw new UnsupportedOperationException();
+        indexerMotor.set(indexerSpeed);
     }
 }
